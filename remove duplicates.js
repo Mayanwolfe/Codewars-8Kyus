8541ad811ae03cfa464222bf425cd65a4700f409
Mayanwolfe <cebourdon625@gmail.com>
2022-04-03 21:42:31 -0400

    codewars file

---
+++ remove duplicates.js
@@ -0,0 +1,11 @@
+// Remove duplicates from list
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// SOLUTION:
+
+function distinct(a) {
+  return Array.from(new Set(a))
+}

e5a0923532c244310829ebfac10fa36feaf982b0
Mayanwolfe <cebourdon625@gmail.com>
2022-04-11 11:29:51 -0400

    codewars file

---
+++ Not so black box.js
@@ -0,0 +1,9 @@
+// Not so black box
+
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+// You may need to think laterally!
+
+// SOLUTION:
+
+blackBox(JSopenSesame());

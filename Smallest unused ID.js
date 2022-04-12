bb23be035cd450ccd1aa16f41cc2874e7251d86c
Mayanwolfe <cebourdon625@gmail.com>
2022-04-12 17:26:12 -0400

    codewars file

---
+++ Smallest unused ID.js
@@ -0,0 +1,23 @@
+// Smallest unused ID
+
+// Hey awesome programmer!
+
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+// Go on and code some pure awesomeness!
+
+// SOLUTION:
+
+function nextId(ids){
+  let i = 0
+  let idSet = new Set(ids.sort(function(a, b){return a - b}))
+  console.log(idSet)
+  while (i == Array.from(idSet)[i]) {
+    i++
+  }
+  return i
+}

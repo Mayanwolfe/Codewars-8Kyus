26b7d71759e51bdf88369874882a171031487f3e
Mayanwolfe <cebourdon625@gmail.com>
2022-04-15 17:05:37 -0400

    codewars file

---
+++ get ascii value.js
@@ -0,0 +1,11 @@
+// get ascii value of character
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// SOLUTION:
+
+function getASCII(c){
+  return c.charCodeAt(0)
+}

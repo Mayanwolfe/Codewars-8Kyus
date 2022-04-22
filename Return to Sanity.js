fff3da6d1edeb485deb952e0f75562ffd282dfdd
Mayanwolfe <cebourdon625@gmail.com>
2022-04-22 10:06:02 -0400

    codewars file

---
+++ Return to Sanity.js
@@ -0,0 +1,9 @@
+//Return to Sanity
+
+//This function should return an object, but it's not doing what's intended. What's wrong?
+
+//SOLUTION:
+
+function mystery() {
+   return {sanity: 'Hello'};
+}

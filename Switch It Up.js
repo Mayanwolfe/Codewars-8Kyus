db856c74c8f70afe92661f2da01f459e35091de1
Mayanwolfe <cebourdon625@gmail.com>
2022-04-26 21:21:41 -0400

    codewars file

---
+++ Switch It Up.js
@@ -0,0 +1,16 @@
+// Switch it Up!
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+// SOLUTION:
+
+function switchItUp(number){
+ let arr = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+ return arr[number]
+  }

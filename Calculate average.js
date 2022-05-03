04f5a56204e4cef655c5616b2e83ecc470150793
Mayanwolfe <cebourdon625@gmail.com>
2022-05-03 19:30:55 -0400

    codewars file

---
+++ Calculate average.js
@@ -0,0 +1,11 @@
+// Calculate average
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+// SOLUTION:
+
+function find_average(array) {
+  return isNaN(array.reduce((sum, current) => sum + current, 0)/array.length) ? 0 : array.reduce((sum, current) => sum + current, 0)/array.length
+}

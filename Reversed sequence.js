7dc3a0a320577c8d6b7bbf760f4dfade6afa4672
Mayanwolfe <cebourdon625@gmail.com>
2022-05-03 19:29:48 -0400

    codewars file

---
+++ Reversed sequence.js
@@ -0,0 +1,16 @@
+// Reversed sequence
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// SOLUTION:
+
+const reverseSeq = n => {
+  let array = []
+  while (n > 0) {
+    array.push(n)
+    n--
+  }
+  return array
+};
